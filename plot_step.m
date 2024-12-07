9a3b6bac83867feeb18f0251097b9e7e0fc34071
Elisavet <echatzikyrka@gmail.com>
2024-12-07 20:12:22 +0200

    Refactor linear system implementation: replace old dynamics function with a new unit system function and update plotting script for step response analysis

--- plot_step.m
+++ plot_step.m
@@ -1,54 +1,56 @@
-% Τιμές Παραμέτρων
-K = 5; % Κέρδος
-T = 0.2; % Χρονική σταθερά
-time_span = [0 3]; % Διάστημα χρόνου προσομοίωσης
-
-% Αρχικές συνθήκες
+K = 5; 
+T = 0.2; 
+time_span = [0 3]; 
 initial_conditions = [-2, 0; 1, 0; 0, 0.5; 2, 2; 2.5, -1; 1.1, 2];
 
-% Βήμα εισόδου
-step_input = 0.5;
-
-% Δημιουργία του figure για τα subplots
-figure;
 num_conditions = size(initial_conditions, 1);
 
+figure;
+
 for i = 1:num_conditions
-    % Προσομοίωση με ode45
-    [t, states] = ode45(@(t, x) linear_system(t, x, K, T, step_input), time_span, initial_conditions(i, :));
+    % Προσομοίωση με ode45 
+    % t = πίνακας με τιμές του χρόνου για τις οποίες υπολογίστηκαν οι λύσεις, 
+    %states = αντίστοιχες τιμές των χ1 και χ2 για καθε στιγμη, πίνακας με δλυο στήλες για χ1 και χ2
+
+    [t, states] = ode45(@(t, x) linear_system_unit(t, x, T, K), time_span, initial_conditions(i, :));
+    %disp(states);
     
-    % Δημιουργία υποδιαγράμματος
-    subplot(ceil(num_conditions / 2), 2, i); % Οργάνωση subplots σε δύο στήλες
-    plot(t, states(:, 1), 'LineWidth', 1.5); % Απόκριση y
+    % figure;
+    % subplot(1, 2, 1);
+    % plot(t, states(:, 1), 'Color', 'green', 'LineStyle', ':', 'LineWidth', 2);
+    % hold on;
+    % plot(t, states(:, 2), 'Color', 'blue', 'LineStyle', '--', 'LineWidth', 2); 
+    % hold off;
+    % xlabel('Χρόνος (s)');
+    % ylabel('Τιμές των x_1 και x_2');
+    % legend('x_1', 'x_2');
+    % title([' Απόκριση των καταστάσεων x_1 και x_2 | ',...
+    %         'x_1(0) = ', num2str(initial_conditions(i, 1)), ', x_2(0) = ', num2str(initial_conditions(i, 2))]);
+    
+    % subplot(1, 2, 2);
+    % plot(states(:, 1), states(:, 2), 'Color', 'red', 'LineWidth', 2);
+    % xlabel('x_1');
+    % ylabel('x_2');
+    % title(['Φασικό Πορτρέτο των καταστάσεων x_1 και x_2 | ',...
+    %         'x_1(0) = ', num2str(initial_conditions(i, 1)), ', x_2(0) = ', num2str(initial_conditions(i, 2))]);
+    % grid on;
+
+    subplot(3, 2, i);
+    yline(0.5, 'Color', 'black', 'LineStyle', ':', 'LineWidth', 2); %r
     hold on;
-    plot(t, step_input * ones(size(t)), '--', 'LineWidth', 1.2); % Αναφορά
+    y = 0.5 - states(:, 1);
+    plot(t, y, 'Color', [1, 0.647, 0], 'LineStyle', '-', 'LineWidth', 2);% y
+    hold off;
     xlabel('Χρόνος (s)');
-    ylabel('Απόκριση');
-    legend('y(t)', 'r(t)', 'Location', 'best');
-    title(['Αρχ. Συνθ.: y(0) = ', num2str(initial_conditions(i, 1)), ', y''(0) = ', num2str(initial_conditions(i, 2))]);
-    grid on;
-
-
-    y_t = states(:, 1); % Απόκριση y στο πεδίο του χρόνου 
-    
-    % FFT για την απόκριση
-    N = length(t); % Αριθμός σημείων
-    dt = mean(diff(t)); % Βήμα χρόνου
-    f = (0:N-1) / (N * dt); % Διανύσματα συχνοτήτων
-    Y_f = abs(fft(y_t)); % Μέτρο του FFT
-
-    % Δημιουργία υποδιαγράμματος για το πεδίο της συχνότητας
-    subplot(num_conditions, 2, 2*i);
-    plot(f, Y_f, 'LineWidth', 1.5); % FFT μέτρο
-    xlabel('Συχνότητα (Hz)');
-    ylabel('|FFT(y)|');
-    title(['FFT: y(0) = ', num2str(initial_conditions(i, 1)), ', y''(0) = ', num2str(initial_conditions(i, 2))]);
-    grid on;
+    ylabel('r, y');
+    legend('r', 'y');
+    title(['x_1(0) = ', num2str(initial_conditions(i, 1)), ', x_2(0) = ', num2str(initial_conditions(i, 2))]);
 
 end
 
-% Προσαρμογή της διάταξης για καλύτερη εμφάνιση
-sgtitle('Αποκρίσεις Συστήματος για Διάφορες Αρχικές Συνθήκες'); % Συνολικός τίτλος
+sgtitle(['Aπόκριση του συστήματος σε βηματική',... 
+            ' είσοδο r = 0.5 και έξοδο y = r - e']);
+
 
 
 
