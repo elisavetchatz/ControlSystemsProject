ba2cc480de2dca1f21ad8d3ce2999ad5a8c0527d
Elisavet <echatzikyrka@gmail.com>
2024-12-22 15:51:39 +0200

    better plot implementation:

--- CHAPTERB/quest_one.m
+++ CHAPTERB/quest_one.m
@@ -8,29 +8,41 @@ initial_conditions = [
    -0.4, 1       
 ];
 
+colors = ['r', 'g', 'b', 'm'];
+
 % Λύση του συστήματος για κάθε αρχική συνθήκη
 figure;
 hold on;
 for i = 1:size(initial_conditions, 1)
     [t, x] = ode45(@system, tspan, initial_conditions(i, :));
-    plot(t, x(:, 1), 'DisplayName', sprintf('x1(%d)', i));
-    plot(t, x(:, 2), '--', 'DisplayName', sprintf('x2(%d)', i));
+    plot(t, x(:, 1), 'DisplayName', sprintf('x1(%d)', i), 'Color', colors(i), 'LineWidth', 2);
+    plot(t, x(:, 2), '--', 'DisplayName', sprintf('x2(%d)', i), 'Color', colors(i), 'LineWidth', 2);
 end
 title('Απόκριση του Συστήματος στο Χρόνο');
 xlabel('Χρόνος (t)');
-ylabel('Μεγέθη x1 και x2');
+ylabel('Τιμές x1 και x2');
 legend;
 grid on;
 hold off;
 
-% Απεικόνιση του πεδίου έλξης στο επίπεδο κατάστασης
+% Φασικό επίπεδο καταστάσεων και πεδίο έλξης
 [x1_values, x2_values] = meshgrid(-2:0.1:2, -2:0.1:2);  
 x1dot = -x1_values + x2_values;
-x2dot = -x1_values + x1_values .* x2_values + 0.5 * x2_values.^2;
+x2dot = -x1_values; % + x1_values .* x2_values + 0.5 * x2_values.^2;
 
 figure;
-quiver(x1_values, x2_values, x1dot, x2dot, 'r');
-title('Πεδίο Έλξης του Συστήματος');
-xlabel('x1');
-ylabel('x2');
+hold on;
+
+quiver(x1_values, x2_values, x1dot, x2dot, 'Color', [0.5, 0.5, 0.5], 'LineWidth', 1.5, 'DisplayName', 'Attraction Field');
+
+for i = 1:size(initial_conditions, 1)
+    [t, x] = ode45(@system, tspan, initial_conditions(i, :)); 
+    plot(x(:, 1), x(:, 2), 'DisplayName', ['x_0 = [' num2str(initial_conditions(i, :)) ']'], 'Color', colors(i), 'LineWidth', 1.5);
+end
+
+xlabel('x_1');
+ylabel('x_2');
+legend;
+title('Φασικό Επίπεδο Καταστάσεων και Πεδίο Έλξης');
 grid on;
+hold off;
